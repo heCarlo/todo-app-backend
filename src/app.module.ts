d1c3ef26b5a7079d6c87ddde01c1fdea384f1329
heCarlo <carlosgfkp@outlook.com>
2024-01-09 14:29:33 -0300

    refactor (app.module): TodoEntity import exclusion

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TodoModule } from './app/todo/todo.module';
-import { TodoEntity } from './app/todo/entity/todo.entity';
 
 @Module({
   imports: [  
