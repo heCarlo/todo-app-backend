f4f2341c63eaef6340695aa5afc02143f8d7c3b7
heCarlo <carlosgfkp@outlook.com>
2024-01-09 17:04:12 -0300

    feat(todo.entity): added ApiProperty to entity attributes

--- src/app/todo/entity/todo.entity.ts
+++ src/app/todo/entity/todo.entity.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { Column, CreateDateColumn, DeleteDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 
 @Entity({ name: 'todos' })
@@ -6,17 +7,22 @@ export class TodoEntity {
     id: string;
 
     @Column()
+    @ApiProperty()
     task: string;
 
     @Column({name: 'is_done', type: 'tinyint', width: 1})
+    @ApiProperty()
     isDone: number;
 
     @CreateDateColumn({name: 'created_at'})
+    @ApiProperty()
     createAt: string;
 
     @UpdateDateColumn({name: 'updated_at'})
+    @ApiProperty()
     updateAt: string;
 
     @DeleteDateColumn({name: 'deleted_at'})
+    @ApiProperty()
     deleteAt: string;
 }
