799c9935f6011cbb467460824d73471fe2d1d0a4
heCarlo <carlosgfkp@outlook.com>
2024-01-09 17:07:19 -0300

    feat(todo.controller): importing swagger settings and return files

--- src/app/todo/todo.controller.ts
+++ src/app/todo/todo.controller.ts
@@ -1,36 +1,113 @@
-import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, ParseUUIDPipe, Post, Put } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  ParseUUIDPipe,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { TodoService } from './todo.service';
 import { get } from 'http';
 import { CreateTodoDto } from './dto/create-todo.dto';
 import { UpdateTodoDto } from './dto/update-todo.dto';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { indexTodoSwagger } from './swagger/index-todo.swagger';
+import { CreateTodoSwagger } from './swagger/create-todo.swagger';
+import { ShowTodoSwagger } from './swagger/show-todo.swagger';
+import { UpdateTodoSwagger } from './swagger/update-todo.swagger';
+import { BadRequestSwagger } from 'src/helpers/swagger/bad-request.swagger';
+import { NotFoundSwagger } from 'src/helpers/swagger/not-found.swagger';
 
 @Controller('api/v1/todos')
+@ApiTags('todos')
 export class TodoController {
-    constructor(private readonly todoService: TodoService) {}
+  constructor(private readonly todoService: TodoService) {}
 
-    @Get()
-    async index() {
-        return await this.todoService.findAll()
-    }
+  @Get()
+  @ApiOperation({ summary: 'Listar todas as tarefas' })
+  @ApiResponse({
+    status: 200,
+    description: 'Lista de tarefas',
+    type: indexTodoSwagger,
+    isArray: true,
+  })
+  async index() {
+    return await this.todoService.findAll();
+  }
 
-    @Post()
-    async create(@Body() body: CreateTodoDto) {
-        return await this.todoService.create(body);
-    }
+  @Post()
+  @ApiOperation({ summary: 'Adicionar uma nova tarefa' })
+  @ApiResponse({
+    status: 201,
+    description: 'Nova tarefa criada com sucesso',
+    type: CreateTodoSwagger,
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Parâmetros inválidos',
+    type: BadRequestSwagger,
+  })
+  async create(@Body() body: CreateTodoDto) {
+    return await this.todoService.create(body);
+  }
 
-    @Get(':id')
-    async show(@Param('id', new ParseUUIDPipe()) id: string) {
-        return await this.todoService.findOneOrFail(id);
-    }
+  @Get(':id')
+  @ApiOperation({ summary: 'Exibir os dados de uma tarefa' })
+  @ApiResponse({
+    status: 200,
+    description: 'Dados de uma tarefa retornado com sucesso',
+    type: ShowTodoSwagger,
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'Tarefa não foi encontrada',
+    type: NotFoundSwagger,
+  })
+  async show(@Param('id', new ParseUUIDPipe()) id: string) {
+    return await this.todoService.findOneOrFail(id);
+  }
 
-    @Put(':id')
-    async update(@Param('id', new ParseUUIDPipe()) id: string, @Body() body: UpdateTodoDto) {
-        return await this.todoService.update(id, body);
-    }
+  @Put(':id')
+  @ApiOperation({ summary: 'Atualizar os dados de uma tarefa' })
+  @ApiResponse({
+    status: 200,
+    description: 'Tarefa atualizada com sucesso',
+    type: UpdateTodoSwagger,
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Dados inválidos',
+    type: BadRequestSwagger,
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'Tarefa não foi encontrada',
+    type: NotFoundSwagger,
+  })
+  async update(
+    @Param('id', new ParseUUIDPipe()) id: string,
+    @Body() body: UpdateTodoDto,
+  ) {
+    return await this.todoService.update(id, body);
+  }
 
-    @Delete(':id')
-    @HttpCode(HttpStatus.NO_CONTENT)
-    async destroy(@Param('id', new ParseUUIDPipe()) id: string) {
-        await this.todoService.deleteById(id);
-    }
+  @Delete(':id')
+  @ApiOperation({ summary: 'Remover uma tarefa' })
+  @ApiResponse({
+    status: 204,
+    description: 'Tarefa removida com sucesso',
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'Tarefa não foi encontrada',
+    type: NotFoundSwagger,
+  })
+  @HttpCode(HttpStatus.NO_CONTENT)
+  async destroy(@Param('id', new ParseUUIDPipe()) id: string) {
+    await this.todoService.deleteById(id);
+  }
 }
